1a2b37e7a6db62220d3ea2ae601383345359723f
soymadip <soumadip@zohomail.in>
2025-09-21 22:30:46 +0530

    finish memory allocation.

--- C/mem-alloc.c
+++ C/mem-alloc.c
@@ -1,26 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main() {
 
-    int *ptr;
+  int *arr;
 
-    ptr = (int *) malloc(5 * sizeof(int));
+  arr = (int *)calloc(5, sizeof(int));
+  if (arr == NULL) {
+    printf("Failed to rallocate. ");
+    free(arr);
+    return 1;
+  }
 
-    ptr[0] = 1;
-    ptr[1] = 2;
-    ptr[2] = 3;
-    ptr[3] = 5;
-    ptr[4] = 8;
-    ptr[5] = 9;
+  arr[0] = 1;
 
-    for (int i = 0; i <6; i++) {
-        printf("index %d: %d.\n", i, ptr[i]);
+  for (int i = 1; i < 5; i++) {
+    arr[i] = arr[i - 1] + 2;
+  }
+
+  printf("Now elements in arary: ");
+
+  for (int i = 0; i < 5; i++) {
+    printf("%d ", arr[i]);
+  }
+
+  printf("\nReallocating for to store even numbers.\n");
+
+  int *tmp = (int *)realloc(arr, 6 * sizeof(int));
+  if (tmp == NULL) {
+    printf("Failed to rallocate. ");
+    free(arr);
+    return 1;
+  } else {
+    arr = tmp;
+  }
+
+  for (int i = 0; i < 6; i++) {
+    if (i == 0) {
+      arr[i] = 2;
+      continue;
     }
+    arr[i] = arr[i-1] + 2;
+  }
 
-    free(ptr);
+  printf("Now elements in arary: ");
+  for (int i = 0; i < 6; i++) {
+    printf("%d ", arr[i]);
+  }
 
-    return 0;
+  printf("\n");
 
+  free(arr);
 }
